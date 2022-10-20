3d550d1deb857f9a10b499cdcd3c1c16880a0baa
SalimmReza <salimnuaa@gmail.com>
2022-10-20 20:51:04 +0600

    done for tday

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 5000
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -13,6 +14,33 @@ app.get('/', (req, res) => {
 
 app.get('/new-categories', (req, res) => {
     res.send(categories)
+})
+
+//news id one only
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
+//all news
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+//category id many
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+    }
+
 })
 
 app.listen(port, () => {
